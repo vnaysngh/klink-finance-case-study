a9e7451d6e08c0eb6b59ac9188ed8b0f08462e64
vnaysngh <singhvinay2525@gmail.com>
2025-03-07 00:43:30 +0530

    mobile responsive implemented for hero section and added swiper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,18 @@
 import Image from "next/image";
-import Kucoin from "@/assets/logos/kucoin.svg";
-import BitPanda from "@/assets/logos/bitpanda.svg";
-import Coinmarketcap from "@/assets/logos/coinmarket.png";
-import Gate from "@/assets/logos/gate.svg";
-import Mexc from "@/assets/logos/mexc.svg";
-import Gecko from "@/assets/logos/gecko.svg";
-import Aerodrome from "@/assets/logos/aerodome.svg";
-import Uniswap from "@/assets/logos/uniswap.svg";
 import Certik from "@/assets/logos/certik.svg";
 import InvestorsSection from "../components/Homepage/TrustedBy";
 import TrustedByTheBest from "../components/Homepage/Backed";
 import Header from "@/layout/Header";
+import ListedOnSwiper from "@/components/ui/swiper";
 
 export default function Hero() {
   return (
     <>
       <section>
-        <Header />
+        <div className="bg-primary relative z-[999] min-h-screen overflow-hidden rounded-4xl pb-6 text-white md:pb-12 lg:min-h-[80vh]">
+          <Header />
 
-        <div className="bg-primary relative z-[999] min-h-screen overflow-hidden rounded-tl-4xl rounded-b-4xl pb-6 text-white md:pb-12 lg:min-h-[80vh]">
-          <div className="spx-6 mx-auto flex flex-col items-center justify-between py-12 lg:flex-row lg:px-12">
+          <div className="mx-auto flex flex-col items-center justify-between px-6 py-12 lg:flex-row lg:px-12">
             <div className="text-center lg:w-1/2 lg:text-left">
               <h1 className="mb-6 text-4xl font-semibold tracking-[-1px] sm:text-4xl md:text-7xl">
                 <div className="text-klink-purple">$KLINK</div>
@@ -35,67 +28,42 @@ export default function Hero() {
               </div>
             </div>
           </div>
-          <div className="mx-auto mt-12 px-6">
-            <p className="text-opacity-70 mb-4 text-base text-white">
-              Audited By
-            </p>
-            <div className="flex flex-wrap gap-4">
-              <div className="flex h-[53.31px] w-[152.16px] items-center justify-center rounded-[16px] border-2 border-[#362B4F] bg-[#2E2746] p-[10px] shadow-lg backdrop-blur-[130px]">
-                <Image
-                  src={Certik}
-                  alt="Exchange Logo"
-                  className="object-contain"
-                />
-              </div>
-              {/* <div className="text-base">4vdfdncd08034309cd0</div> */}
-            </div>
-          </div>
-          <div className="mx-auto mt-12 px-6">
-            <p className="text-opacity-70 mb-4 text-base text-white">
-              $KLINK Listed On
-            </p>
-            <div className="flex flex-wrap gap-4">
-              {[
-                Kucoin,
-                Mexc,
-                Gate,
-                BitPanda,
-                Uniswap,
-                Aerodrome,
-                Coinmarketcap,
-                Gecko,
-              ].map((logo, index) => (
-                <div
-                  key={index}
-                  className="flex h-[53.31px] w-[152.16px] items-center justify-center rounded-[16px] border-2 border-[#362B4F] bg-[#2E2746] p-[10px] shadow-lg backdrop-blur-[130px]"
-                >
+          <div className="absolute bottom-8 w-full md:relative md:bottom-5">
+            <div className="mx-auto mt-12 px-6 lg:px-12">
+              <p className="mb-4 text-base text-white opacity-70">Audited By</p>
+              <div className="flex flex-wrap gap-4">
+                <div className="flex h-[53.31px] w-[152.16px] items-center justify-center rounded-[16px] border-2 border-[#362B4F] bg-[#2E2746] p-[10px] shadow-lg backdrop-blur-[130px]">
                   <Image
-                    src={logo}
+                    src={Certik}
                     alt="Exchange Logo"
                     className="object-contain"
                   />
                 </div>
-              ))}
+                {/* <div className="text-base">4vdfdncd08034309cd0</div> */}
+              </div>
             </div>
+            <ListedOnSwiper />
           </div>
+
           <video
-            className="absolute right-[-25%] bottom-[-50%] z-[-1] mx-auto w-full max-w-6xl rounded-br-4xl shadow-lg"
+            className="absolute bottom-[-35%] z-[-1] mx-auto w-full max-w-6xl scale-175 overflow-hidden rounded-br-4xl shadow-lg md:bottom-[-55%] md:scale-100 lg:right-[-20%]"
             autoPlay
             loop
             muted
             playsInline
+            poster="https://klink-cdn.klink.finance/ecosystem/klink_coin.webp"
           >
             <source
               src="https://klink-cdn.klink.finance/ecosystem/klink_coin.webm"
-              type="video/webm"
+              type="video/mp4"
             />
             Your browser does not support the video tag.
           </video>
         </div>
       </section>
-      <TrustedByTheBest />
+      {/* <TrustedByTheBest />
 
-      <InvestorsSection />
+      <InvestorsSection /> */}
     </>
   );
 }
---
+++ src/components/ui/swiper.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Autoplay } from "swiper/modules";
+import Image from "next/image";
+import Kucoin from "@/assets/logos/kucoin.svg";
+import BitPanda from "@/assets/logos/bitpanda.svg";
+import Coinmarketcap from "@/assets/logos/coinmarket.png";
+import Gate from "@/assets/logos/gate.svg";
+import Mexc from "@/assets/logos/mexc.svg";
+import Gecko from "@/assets/logos/gecko.svg";
+import Aerodrome from "@/assets/logos/aerodome.svg";
+import Uniswap from "@/assets/logos/uniswap.svg";
+
+const logos = [
+  Kucoin,
+  Gate,
+  BitPanda,
+  Uniswap,
+  Mexc,
+  Aerodrome,
+  Coinmarketcap,
+  Gecko,
+];
+
+export default function ListedOnSwiper() {
+  return (
+    <div className="mx-auto mt-12 px-3 lg:px-12">
+      <p className="mb-4 text-center text-base text-white opacity-70 md:text-left">
+        $KLINK Listed On
+      </p>
+      <Swiper
+        slidesPerView={3.25} // Show 3.5 logos on mobile
+        breakpoints={{
+          640: { slidesPerView: 8 }, // 4 on medium screens
+          1024: { slidesPerView: 8 }, // 5 on larger screens
+        }}
+        loop={true}
+        autoplay={{ delay: 2000 }}
+        modules={[Autoplay]}
+      >
+        {logos.map((logo, index) => (
+          <SwiperSlide key={index} className="flex justify-center">
+            <div className="flex h-[40px] w-[100px] items-center justify-center rounded-[12px] border border-[#362B4F] bg-[#2E2746] p-[5px] shadow-lg backdrop-blur-[130px] sm:h-[50px] sm:w-[130px] sm:p-[10px]">
+              <Image src={logo} alt="Exchange Logo" />
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+}
