e076e541d2a90f870ea83df2245fda54e84a3ded
vnaysngh <singhvinay2525@gmail.com>
2025-03-10 19:20:20 +0530

    hydration bug fix

--- src/layouts/header/Menu.tsx
+++ src/layouts/header/Menu.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import { metaMask } from "wagmi/connectors";
 import { Price } from "@/app/components/ui/cards/price";
@@ -16,9 +16,16 @@ export default function Menu({ toggleMenu }: MenuProps) {
   const { connect } = useConnect();
   const { disconnect } = useDisconnect();
   const account = useAccount();
+  const [isClient, setIsClient] = useState(false);
 
-  const renderWalletButton = () =>
-    account?.address ? (
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  const renderWalletButton = () => {
+    if (!isClient) return null; // Prevent rendering on the server
+
+    return account?.address ? (
       <button
         onClick={() => disconnect()}
         className="bg-opacity-75 flex items-center space-x-2 rounded-full border border-[#9A8AFE] bg-gradient-to-r from-[#674EFF] to-[#714EBD] px-3 py-1.5 text-sm text-white backdrop-blur-sm"
@@ -33,6 +40,7 @@ export default function Menu({ toggleMenu }: MenuProps) {
         Connect
       </button>
     );
+  };
 
   return (
     <div className="relative flex h-20 items-center justify-between rounded-tr-4xl px-4 md:pl-16 lg:flex-1">
@@ -74,7 +82,7 @@ export default function Menu({ toggleMenu }: MenuProps) {
 
       <div className="hidden items-center space-x-6 lg:flex">
         <Price />
-        <Wallet />
+        {isClient && <Wallet />}
       </div>
     </div>
   );
