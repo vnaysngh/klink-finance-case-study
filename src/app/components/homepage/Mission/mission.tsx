fc71a79b6ac197589ce93c557a82953847e292f5
vnaysngh <singhvinay2525@gmail.com>
2025-03-10 21:19:02 +0530

    vercel build fix

--- src/app/components/homepage/Mission/mission.tsx
+++ src/app/components/homepage/Mission/mission.tsx
@@ -1,3 +1,4 @@
+"use client";
 import dynamic from "next/dynamic";
 import { MissionLottie } from "../../ui/lotties";
 const PartnerGrid = dynamic(() => import("./PartnerGrid"), {
--- src/app/components/homepage/trusted/index.tsx
+++ src/app/components/homepage/trusted/index.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import Testimonials from "../../ui/swiper/testimonials";
 import { SparkLottie } from "../../ui/lotties";
