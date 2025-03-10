972df32750313a9e993496f3211862d639d59435
vnaysngh <singhvinay2525@gmail.com>
2025-03-10 15:51:06 +0530

    file import fix

--- src/app/components/homepage/Mission/index.tsx
+++ src/app/components/homepage/Mission/index.tsx
@@ -3,14 +3,14 @@ import MissionGrid from "./MissionGrid";
 import { MissionLottie } from "../../ui/lotties";
 import StatCounter from "./StatsCounter";
 
-export default function Home() {
+export default function Mission() {
   return (
     <div className="relative overflow-hidden">
       <MissionLottie />
       <div className="bg-background relative mx-auto my-12 min-h-screen w-full max-w-6xl py-12">
-        <div className="z-15 relative">
+        <div className="relative z-15">
           <div className="bg-muted/30 w-full py-4">
-            <div className="text-muted-foreground text-heading container mx-auto text-center text-2xl italic tracking-tighter opacity-60">
+            <div className="text-muted-foreground text-heading container mx-auto text-center text-2xl tracking-tighter italic opacity-60">
               Trusted by the best in Web3
             </div>
           </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
-import InvestorsSection from "./components/homepage/trusted";
+import TrustedByTheBest from "./components/homepage/trusted";
 import Hero from "./components/homepage/hero";
 import Footer from "@/layouts/footer";
-import TrustedByTheBest from "./components/homepage/mission";
+import Mission from "./components/homepage/mission";
 
 export default function Page() {
   return (
     <>
       <section className="mx-auto p-3 sm:pt-4">
         <Hero />
-        <TrustedByTheBest />
+        <Mission />
       </section>
       <section className="p-0 sm:px-4">
-        <InvestorsSection />
+        <TrustedByTheBest />
       </section>
       <Footer />
     </>
