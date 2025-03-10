ccbe27aeae2507c90ab18b6347c588bc1b182ce3
vnaysngh <singhvinay2525@gmail.com>
2025-03-10 22:20:31 +0530

    continuous debug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,37 @@
 "use client";
 import Footer from "@/layouts/footer";
-import Hero from "./components/homepage/hero";
-import TrustedByTheBest from "./components/homepage/trusted";
-import Mission from "./components/homepage/Mission/mission";
-import { useEffect, useState } from "react";
+import dynamic from "next/dynamic";
+
+const Hero = dynamic(() => import("./components/homepage/hero"), {
+  ssr: false,
+});
+
+const TrustedByTheBest = dynamic(
+  () => import("./components/homepage/trusted"),
+  {
+    ssr: false,
+  },
+);
+
+const Mission = dynamic(() => import("./components/homepage/Mission/mission"), {
+  ssr: false,
+});
 
 const Page = () => {
-  const [isClient, setIsClient] = useState(false);
-
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
-
-  if (isClient) {
-    return (
+  return (
+    <>
+      <section className="mx-auto p-3 sm:pt-4">
+        <Hero />
+        <Mission />
+      </section>
       <>
-        <section className="mx-auto p-3 sm:pt-4">
-          <Hero />
-          <Mission />
+        <section className="p-0 sm:px-4">
+          <TrustedByTheBest />
         </section>
-        {isClient && (
-          <>
-            <section className="p-0 sm:px-4">
-              <TrustedByTheBest />
-            </section>
-            <Footer />
-          </>
-        )}
+        <Footer />
       </>
-    );
-  }
-
-  return null;
+    </>
+  );
 };
 
 export default Page;
